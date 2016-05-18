a7f7d53a296f472b80a5c8b08286abcc1d64e6a1
Andrew Chambers <andrewrchambers@gmail.com>
2016-05-18 18:54:25 +0100

    Adding main python script

---
+++ pool.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return "POOL!";
+    
+if __name__ == '__main__':
+    app.run(debug=True)
